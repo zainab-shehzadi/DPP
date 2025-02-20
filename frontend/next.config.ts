e2744bf295c2d2c70cc8d1f6389ce1a9f07c7178
zainab-shehzadi <zainabdev@paklogics.com>
2025-02-20 23:56:49 +0500

    update file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true, // Disable ESLint rules in production builds
   },
   /* other config options here */
+  
 };
 
 export default nextConfig;
+
+module.exports = {
+  env: {
+    API_URL: process.env.NODE_ENV === "production" 
+      ? "https://209.105.243.7:5000/" 
+      : "http://209.105.243.7:5000/"
+  }
+}
